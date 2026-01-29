54ff9cd7b82130edca086506a18b3f561ec70965
Axel <nigthmaresans2@gmail.com>
2026-01-28 22:20:37 -0600

    page output

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
 from notion import Notion, info
+import json
 
 api = open(".api_key", "r").read()
 
 n = Notion()
 n.set_bearer_api_key(api)
 
-info(n.get_database("2f6a67c3b14781879f14d8fe33ca5bb0")["data_sources"])
+sources = n.get_database("2f6a67c3b14781879f14d8fe33ca5bb0")["data_sources"]
+
+for src in sources:
+    pages = n.get_pages_from_datasource(src["id"])
+    for page in pages["results"]:
+        props = page["properties"]
+        info("Page id:", page["id"])
+        info("Name:", props["Name"]["title"][0]["plain_text"])
+        info("Status:", props["Status"].get("status", {}).get("name"))
+        
+        date = props["Due date"]["date"]
+        if date:
+            info("Due date:", date.get("end"))
+        else:
+            info("No due date")
+        info("Priority:", props["Priority"]["select"])
+
+        info("Description:", props["Description"]["rich_text"][0]["plain_text"])
+        print("\n")
--- src/notion.py
+++ src/notion.py
@@ -28,9 +28,20 @@ class Notion:
     def set_notion_version(self, version: str):
         self.headers["Notion-Version"] = version
     
-    def request(self, func, method, *args, **kwargs):
-        return func(self.API + method, *args, headers=self.headers, **kwargs)
+    def request(self, func, method, *args, **kwargs) -> dict:
+        return func(self.API + method, *args, headers=self.headers, **kwargs).json()
 
     def get_database(self, database_id: str):
-        res = self.request(requests.get, f"/databases/{database_id}")
-        return res.json()
+        return self.request(requests.get, f"/databases/{database_id}")
+    
+    def get_pages_from_datasource(self, datasource_id:str):
+        return self.request(requests.post, f"/data_sources/{datasource_id}/query")
+
+    def get_datasource(self, datasource_id: str):
+        return self.request(requests.get, f"/data_sources/{datasource_id}")
+
+    def get_page(self, page_id: str):
+        return self.request(requests.get, f"/pages/{page_id}")
+
+    def get_block(self, block_id: str):
+        ...
